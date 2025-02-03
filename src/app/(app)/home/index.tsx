32020458c1d95950fec976ecb8e74a66feb79bcb
Cleby Francisco <clebysilva5@gmail.com>
2025-02-03 08:52:37 -0300

    fix: componente cards

--- src/app/(app)/home/index.tsx
+++ src/app/(app)/home/index.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import { useSession } from '@/contexts/AuthContext';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
-import { styles } from './styles';
+import { LinearGradient } from 'expo-linear-gradient';
 import { getGreeting } from '@/utils/greeting';
 import { useDialog, DialogContent } from '@/components/Dialog';
 import { Input } from '@/components/Input';
 import { colors } from '@/styles/colors';
+import { Card } from '@/components/atoms/Card';
+import { CardSecondary } from '@/components/atoms/CardSecondary';
 
 
 export default function Home() {
-    const { userInfo } = useSession();
+    // const { userInfo } = useSession();
+
+    const userInfo = {
+        name: "John",
+    }
     const { setOpen } = useDialog();
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
@@ -29,66 +35,83 @@ export default function Home() {
         });
 
         if (!result.canceled) {
-            setSelectedImage(result.assets[0].uri); 
+            setSelectedImage(result.assets[0].uri);
         }
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.mainCard}>
-                <View style={styles.greetingContainer}>
-                    <Text style={styles.greetingText}>
-                        {getGreeting()}, {userInfo?.name.toUpperCase()}!
-                    </Text>
-                    <Text style={styles.mainText}>New Day, New Strength!</Text>
-                    <TouchableOpacity style={styles.studyButton}>
-                        <Text style={styles.studyButtonText}>Study Now</Text>
-                        <MaterialIcons name="arrow-forward" size={24} color={colors.primary[500]} />
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-
-            <TouchableOpacity style={styles.floatingButton} onPress={() => setOpen(true)}>
-                <MaterialIcons name="add" size={40} color={colors.gray[100]} />
-            </TouchableOpacity>
-
-            <DialogContent className="bg-white rounded-t-lg w-full absolute flex items-center bottom-0 h-1/2 p-4">
-                {/* Cabeçalho do Dialog */}
-                <View className="flex flex-row justify-between items-center mb-2 w-full">
-                    <Text className="font-semibold text-xl text-primary justify-center">New deck collection</Text>
-                    <TouchableOpacity onPress={() => setOpen(false)}>
-                        <MaterialIcons name="close" size={24} color={colors.gray[950]} />
-                    </TouchableOpacity>
+        <View className='flex-1 w-4/5 max-w-[1440px] mx-auto mt-12 relative'>
+            <ScrollView contentContainerStyle={{ paddingBottom: 100 }} showsVerticalScrollIndicator={false}>
+                <View >
+                    <View className='flex flex-col justify-center items-center py-5'>
+                        <Text className='text-[15px] text-gray-600 mb-1 font-[ComicSans]'>
+                            {getGreeting()}, {userInfo?.name.toUpperCase()}!
+                        </Text>
+                        <Text style={{ color: colors.primary[500] }}
+                            className="text-2xl font-bold">New Day, New Strength!</Text>
+                    </View>
+                    <Card />
                 </View>
 
-                <View className="border-b border-gray-300 mb-4 w-full" />
+                <Text style={{ color: colors.primary[600] }}
+                    className="text-sm font-bold my-7">CHECK OUT OTHERS MASTERDECKS</Text>
 
-                <TouchableOpacity
-                    onPress={pickImage}
-                    className="border border-dashed border-gray-400 rounded-lg p-10 flex items-center justify-center w-full"
-                >
-                    {selectedImage ? (
-                        <View className='relative'>
-                            <Image source={{ uri: selectedImage }} className="w-32 h-32 rounded-lg" />
-                            <View className='bg-slate-100 absolute -top-2 -right-2 w-6 rounded-md' >
-                                <MaterialIcons onPress={() => setSelectedImage(null)} name="close" size={24} color="red" className='' />
+                <CardSecondary />
+                <CardSecondary />
+                <CardSecondary />
+                <CardSecondary />
+
+                </ScrollView>
+
+                <LinearGradient 
+                colors={['transparent', 'white']} 
+                className="absolute bottom-0 left-0 right-0 h-28" 
+                pointerEvents="none" 
+            />
+
+
+                <TouchableOpacity className="absolute bottom-7 right-7 bg-[#007AFF] rounded-full p-2.5"
+                    onPress={() => setOpen(true)}>
+                    <MaterialIcons name="add" size={40} color={colors.gray[100]} />
+                </TouchableOpacity>
+
+                <DialogContent className="bg-white rounded-t-lg w-full absolute flex items-center bottom-0 h-1/2 p-4">
+                    {/* Cabeçalho do Dialog */}
+                    <View className="flex flex-row justify-between items-center mb-2 w-full">
+                        <Text className="font-semibold text-xl text-primary justify-center">New deck collection</Text>
+                        <TouchableOpacity onPress={() => setOpen(false)}>
+                            <MaterialIcons name="close" size={24} color={colors.gray[950]} />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View className="border-b border-gray-300 mb-4 w-full" />
+
+                    <TouchableOpacity
+                        onPress={pickImage}
+                        className="border border-dashed border-gray-400 rounded-lg p-10 flex items-center justify-center w-full"
+                    >
+                        {selectedImage ? (
+                            <View className='relative'>
+                                <Image source={{ uri: selectedImage }} className="w-32 h-32 rounded-lg" />
+                                <View className='bg-slate-100 absolute -top-2 -right-2 w-6 rounded-md' >
+                                    <MaterialIcons onPress={() => setSelectedImage(null)} name="close" size={24} color="red" className='' />
+                                </View>
                             </View>
-                        </View>
 
-                    ) : (
-                        <View className='flex-col items-center justify-center '>
-                            <MaterialIcons name="cloud-upload" size={40} color="gray" />
-                            <Text className="text-gray-500 mt-2 ">Tap to send an image</Text>
-                        </View>
-                    )}
-                </TouchableOpacity>
-                <Input placeholder="Enter your name deck collection" className='py-6 w-full' />
-                <TouchableOpacity style={{backgroundColor:colors.primary[500]}}  className='w-full max-w-[500px] py-4 rounded-3xl items-center mb-5' onPress={() => console.log()}>
-                    <Text className='text-white text-base font-bold'>Create New deck collection</Text>
-                </TouchableOpacity>
+                        ) : (
+                            <View className='flex-col items-center justify-center '>
+                                <MaterialIcons name="cloud-upload" size={40} color="gray" />
+                                <Text className="text-gray-500 mt-2 ">Tap to send an image</Text>
+                            </View>
+                        )}
+                    </TouchableOpacity>
+                    <Input placeholder="Enter your name deck collection" className='py-6 w-full' />
+                    <TouchableOpacity style={{ backgroundColor: colors.primary[500] }} className='w-full max-w-[500px] py-4 rounded-3xl items-center mb-5' onPress={() => console.log()}>
+                        <Text className='text-white text-base font-bold'>Create New deck collection</Text>
+                    </TouchableOpacity>
 
-            </DialogContent>
+                </DialogContent>
+            
         </View>
     );
 }
---
+++ src/components/atoms/CardSecondary.tsx
@@ -0,0 +1,30 @@
+import { View, Image, Text } from "react-native";
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { colors } from "@/styles/colors";
+
+export const CardSecondary = () => {
+    return (
+        <View className="w-[300px] h-[100px] bg-white rounded-[12px] overflow-hidden relative shadow-lg my-3">
+
+            <Image
+                source={require('@/assets/logo_memobelc.jpg')}
+                className="w-[30%] h-full absolute  top-0"
+            />
+
+            <View className="w-[70%] h-full justify-center items-center left-[30%]">
+                <Text className="text-xl font-bold pb-3">Master Deck</Text>
+                <View className="flex flex-row justify-between items-center  w-[80%]">
+                    <View className="flex  bg-red-100 px-2 py-1 rounded-md items-center justify-between flex-row"
+                    >
+                        <MaterialCommunityIcons className="pr-2" name="cards" size={24} color={colors.red[600]} />
+                        <Text className="text-xs  color-red-700">1000 out of 1200</Text>
+                    </View>
+                    <MaterialIcons name="language" size={24} color="#000" />
+                </View>
+
+
+            </View>
+        </View>
+    );
+};
