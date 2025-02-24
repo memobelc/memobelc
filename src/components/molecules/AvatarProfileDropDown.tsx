7080cbe39cb21e8c4d8e755366a175f43b4e5fc3
Cleby Francisco <clebysilva5@gmail.com>
2025-02-24 09:39:17 -0300

    fix: Avatar profile sensive dropdown

--- src/components/molecules/AvatarProfileDropDown.tsx
+++ src/components/molecules/AvatarProfileDropDown.tsx
@@ -1,70 +1,145 @@
-import { View, Image, TouchableOpacity, Text } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import { useSession } from '@/contexts/AuthContext';
-
 import {
-  DropDown,
-  DropDownContent,
-  DropDownItem,
-  DropDownItemSeparator,
-  DropDownTrigger,
-} from '@/components/DropDown';
-
+  View,
+  TouchableOpacity,
+  Text,
+  Modal,
+  TouchableWithoutFeedback,
+  Animated,
+  Pressable,
+  Platform,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import React, { useEffect, useRef, useState } from 'react';
+import { usePathname, useRouter } from 'expo-router';
+import { useSession } from '@/contexts/AuthContext';
 import { Avatar, AvatarImage } from '@/components/Avatar';
+
+type menuItem = {
+  name: string;
+  path: string;
+  icon: React.ReactNode;
+  disabled: boolean;
+};
+
+const menuItems: menuItem[] | [] = [];
 
 const AvatarProfileDropDown = () => {
   const { userInfo, signOut } = useSession();
 
-  return (
-    <TouchableOpacity>
-      <DropDown>
-        <DropDownTrigger>
-          <TouchableOpacity>
-            <Avatar>
-              <AvatarImage
-                source={
-                  userInfo?.image
-                    ? {
-                        uri: userInfo?.image,
-                      }
-                    : require('@/assets/fallback.png')
-                }
-              />
-            </Avatar>
-          </TouchableOpacity>
-        </DropDownTrigger>
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
-        <DropDownContent>
-          <DropDownItem>
-            <TouchableOpacity className="flex flex-row gap-2 items-center">
-              <Avatar>
-                <AvatarImage
-                  source={
-                    userInfo?.image
-                      ? {
-                          uri: userInfo?.image,
-                        }
-                      : require('@/assets/fallback.png')
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const translateX = useRef(new Animated.Value(-300)).current;
+
+  useEffect(() => {
+    if (open) {
+      Animated.timing(translateX, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(translateX, {
+        toValue: 300,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [open]);
+  return (
+    <View>
+      <TouchableOpacity onPress={() => setOpen(true)}>
+        <Avatar>
+          <AvatarImage
+            source={
+              userInfo?.image
+                ? {
+                    uri: userInfo?.image,
                   }
-                />
-              </Avatar>
+                : require('@/assets/fallback.png')
+            }
+          />
+        </Avatar>
+      </TouchableOpacity>
+      <Modal
+        transparent
+        animationType="fade"
+        visible={open}
+        onRequestClose={handleClose}
+      >
+        <TouchableWithoutFeedback onPress={handleClose}>
+          <View className="flex-1 justify-center relative items-center">
+            <Animated.View
+              style={{
+                transform: [{ translateX }],
+              }}
+              className="bg-white w-[50%]  absolute right-0 top-0 p-4 rounded-l-2xl"
+            >
+              <TouchableOpacity className="flex flex-row gap-2 items-center mb-3">
+                <Avatar>
+                  <AvatarImage
+                    source={
+                      userInfo?.image
+                        ? {
+                            uri: userInfo?.image,
+                          }
+                        : require('@/assets/fallback.png')
+                    }
+                  />
+                </Avatar>
+                <View>
+                  <Text className="text-primary text-xs">{userInfo?.name}</Text>
+                  <Text className="text-primary text-xs">
+                    {userInfo?.email}
+                  </Text>
+                </View>
+              </TouchableOpacity>
               <View>
-                <Text className="text-primary text-xs">{userInfo?.name}</Text>
-                <Text className="text-primary text-xs">{userInfo?.email}</Text>
+                {menuItems &&
+                  menuItems.map((item) => {
+                    const isActive = pathname === item.path;
+                    return (
+                      <Pressable
+                        key={item.name}
+                        className={`flex-row mb-3 -left-4 w-[70%] px-5 py-1 rounded-r-3xl${
+                          isActive ? 'bg-orange-400' : ''
+                        }`}
+                        disabled={item.disabled}
+                        onPress={() => {
+                          router.push(item.path as `./${string}`);
+                          handleClose();
+                        }}
+                        {...(Platform.OS === 'web'
+                          ? { onMouseLeave: () => {} }
+                          : {})}
+                      >
+                        {item.icon}
+                        <Text
+                          className={`font-[ComicSans] ml-3 ${item.disabled ? 'text-gray-300' : 'text-primary-600'}`}
+                        >
+                          {item.name}
+                        </Text>
+                      </Pressable>
+                    );
+                  })}
               </View>
-            </TouchableOpacity>
-          </DropDownItem>
-          <DropDownItemSeparator />
-          <DropDownItem
-            onPress={signOut}
-            className="flex flex-row gap-2 items-center cursor-pointer"
-          >
-            <MaterialIcons name="logout" size={20} />
-            <Text className="text-primary text-xs">Log out</Text>
-          </DropDownItem>
-        </DropDownContent>
-      </DropDown>
-    </TouchableOpacity>
+              <TouchableOpacity
+                onPress={signOut}
+                className="flex flex-row gap-2 items-center cursor-pointer"
+              >
+                <MaterialIcons name="logout" size={20} />
+                <Text className="text-primary text-xs">Log out</Text>
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </View>
   );
 };
 
